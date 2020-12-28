67257195478aca827e8813ef2e8aba29dd99ad07
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-12-27 21:23:32 -0600

    added another problem

---
+++ js/student-problem.js
@@ -0,0 +1,13 @@
+
+const studentArr = [
+    [58, "algebra"],
+    [58, "athletics"],
+    [63, "art"],
+    [88, "algebra"],
+    [72, "english"],
+    [77, "calculus"],
+    [69, "art"]
+];
+
+//TODO: Write a function that returns which students from the StudentArr share the same classes.  Students are identified by their id numbers.  Use any data structure.
+
