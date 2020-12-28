b687a1f197ab6ea15f220ff368de739f6b9adb3e
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-12-27 21:00:15 -0600

    github api problem

---
+++ js/github-api-call.js
@@ -0,0 +1,4 @@
+
+//TODO: Write a function(s) to find the frequency of languages used in a user's github repos.  Use the github API- https://api.github.com/users/{username}/repos
+// EX: https://api.github.com/users/jasmineannrivera/repos => HTML-5, Java-9, JavaScript-6, TSQL-1, null-5
+
